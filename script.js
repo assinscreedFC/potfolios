a780fc7c777215b2ea2625f5325669ea4ebc6118
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-06-18 14:54:15 +0100

    css stylle :)

--- script.js
+++ script.js
@@ -2,23 +2,28 @@ const cont=document.querySelector(".container");
 const image=cont.querySelectorAll(".icone");
 
 
-async function setTEmps(code,svgContainer) {
+// async function setTEmps(code,svgContainer) {
     
-    try{
-      const    response    =    await fetch(`./assets/${code}`)
+//     try{
+//       const    response    =    await fetch(`./assets/${code}`)
       
-        if (!response.ok) {
-          throw new Error("Network response was not ok " + response.statusText);
-        }
-        const svgContent = await response.text();
-        svgContainer.forEach((element) => {
-            element.innerHTML += svgContent;
-            console.log(element);
-        });
-    }
-     catch(error) {
-        console.error("Erreur lors du chargement du SVG:", error);
-      }
-    } setTEmps("github.svg",image);
-    setTEmps("share-all.svg",image);
+//         if (!response.ok) {
+//           throw new Error("Network response was not ok " + response.statusText);
+//         }
+//         const svgContent = await response.text();
+//         svgContainer.forEach((element) => {
+//             element.innerHTML += svgContent;
+//             console.log(element);
+//         });
+//     }
+//      catch(error) {
+//         console.error("Erreur lors du chargement du SVG:", error);
+//       }
+//     } setTEmps("github.svg",image);
+//     setTEmps("share-all.svg",image);
+
+image.forEach(element => {
+  element.innerHTML+="<img src=\"./assets/github.svg\" alt=\"\" width=\"34\"  height=\"24\" >"
+  element.innerHTML+="<img src=\"./assets/share-all.svg\" alt=\"\" width=\"34\"  height=\"24\">"
+});
    
